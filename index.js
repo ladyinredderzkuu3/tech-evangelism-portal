b2b85b9b5d94201ccabf5dd0c0239339460f567f
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-09-03 12:04:59 -0500

    Update welcome message for clarity

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Technology Evangelism Portal!');
+    res.send('Welcome to the Tech Evangelism Portal. We promote innovation and technology!');
 });
 
 app.listen(PORT, () => {
